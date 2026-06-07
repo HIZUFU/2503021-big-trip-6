42e5c09c813b2a62f009443cfbedc906a29f414f
Андрей Дианов <a.d200333@gmail.com>
2026-06-07 13:08:36 +0500

    Add event presenter and favorite update

--- src/model/trip-model.js
+++ src/model/trip-model.js
@@ -20,4 +20,20 @@ export default class TripModel {
   get offers() {
     return this.#offers;
   }
+
+  updatePoint(updatedPoint) {
+    const pointIndex = this.#points.findIndex(
+      (point) => point.id === updatedPoint.id
+    );
+
+    if (pointIndex === -1) {
+      throw new Error('Can not update unexisting point');
+    }
+
+    this.#points = [
+      ...this.#points.slice(0, pointIndex),
+      updatedPoint,
+      ...this.#points.slice(pointIndex + 1),
+    ];
+  }
 }
